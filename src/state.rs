156f47fb7dda6c6abfcd47fc98097efdcd211c4d
Ali Imran <ali.adnani@hotmail.com>
2026-09-22 21:53:52 +0800

    debug: fix i2s device

--- src/state.rs
+++ src/state.rs
@@ -1,1 +1,22 @@
+pub struct State {
+    nam_models: Vec<ValidNamA2ModelPath>,
+    current_model_index: Option<usize>,
+    bypass_enabled: bool,
+}
 
+pub struct ValidNamA2ModelPath(String);
+
+impl ValidNamA2ModelPath {
+    pub fn new(path: String) -> Result<Self, String> {
+        // TODO: Implement actual validation logic for the NAM A2 model;
+        // 1. Check the file actually exists
+        // 2. Check it ends with .nam
+        // 3. NAM models are just JSON files, try parse and assert on some of its properties
+        //   3.1 For NAM A2 specifically, assert that it's architercture is indeed SlimmableContainer (i.e. A2) 
+        Ok(ValidNamA2ModelPath(path))
+    }
+
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
+}
